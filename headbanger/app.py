a58f727cb8f85c2993b3ad480932becb9126bc20
Sergey <xxsmotur@gmail.com>
2020-06-28 18:20:18 +0500

    reorganization with creating app

--- headbanger/app.py
+++ headbanger/app.py
@@ -1,28 +1,26 @@
 from flask import Flask, jsonify, send_from_directory
 from flask_cors import CORS
+from core.git import Git
 
 # https://tproger.ru/translations/developing-app-with-flask-and-vue-js/
 # https://ru.vuejs.org/v2/guide/index.html
-app = Flask(__name__, static_folder='static/dist')
-CORS(app)
 
-@app.route('/')
-def index():
-    return app.send_static_file("index.html")
+def create_app(congig=None):
+    app = Flask(__name__, static_folder='static/dist')
+    CORS(app)
 
-@app.route('/dist/<path:path>')
-def static_dist(path):
-    return send_from_directory("static/dist", path)
+def configure_api(app:Flask):
 
+    @app.route('/api/data', methods=['GET'])
+    def languages():
+        return jsonify({
+            'branches': [
+                {'name': 'assembly'},
+                {'name': 'c#'},
+                ]
+            })
 
-@app.route('/api/data', methods=['GET'])
-def languages():
-    return jsonify({
-        'branches': [
-            {'name': 'assembly'},
-            {'name': 'c#'},
-        ]
-    })
+#'/home/motorcode/headbanger_repo')
 
 if __name__ == '__main__':
-    app.run()
+    create_app()
